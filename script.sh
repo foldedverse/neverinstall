670d3519337812456b92ae0a45f411d1f179f878
a <a@b.com>
2022-12-23 20:17:43 +0530

    ini

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
-cp id_rsa ~/.ssh/id_rsa 
+mkdir ~/.ssh
+cp start/id_rsa ~/.ssh/id_rsa 
+chmod 600 ~/.ssh/id_rsa 
 git config --global user.name a
 git config --global user.email a@b.com
-cd ..
 git remote set-url origin git@github.com:foldedverse/neverinstall.git
