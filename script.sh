4403c141fd4a6d3941495cb1f342bf5d08b0fb56
a <a@b.com>
2022-12-23 20:34:20 +0530

    d

--- script.sh
+++ script.sh
@@ -4,3 +4,6 @@ chmod 600 ~/.ssh/id_rsa
 git config --global user.name a
 git config --global user.email a@b.com
 git remote set-url origin git@github.com:foldedverse/neverinstall.git
+
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
